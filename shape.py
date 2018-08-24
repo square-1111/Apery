f22397cfa231e91db2491f42c5bc073debb08665
square-1111 <tezuesh.varshney@gmail.com>
2018-08-24 23:36:10 +0530

    Refactoring Shape.py

--- shape.py
+++ shape.py
@@ -2,8 +2,12 @@ from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import random
 
+class Shape(object):
+    # def __new__(cls, *args, **kwargs):
+    #             #print "Creating Instance"
+    #     instance = super()(Circle, cls).__new__(cls, *args, **kwargs)
+    #     return instance
 
-class Circle():
 
     def __init__(self):
         self.red = random.randint(0,255)
@@ -17,64 +21,61 @@ class Circle():
 
     def draw(self, base):
         #canvas of size 350 * 350
+        global drw
         drw = ImageDraw.Draw(base,'RGBA')
+        global coordinate
         coordinate = [(self.x1, self.y1),(self.x2, self.y2)]
+        global color
         color = (self.red, self.green, self.blue, self.alpha)
+
+
+class Circle(Shape):
+
+    def __init__(self):
+        super().__init__()
+
+    def draw(self, base):
+        super().draw(base)
         drw.ellipse(coordinate, color, None)
         return base
 
-class Rectangle():
+class Rectangle(Shape):
 
     def __init__(self):
-        self.red = random.randint(0,255)
-        self.blue = random.randint(0,255)
-        self.green = random.randint(0,255)
-        self.alpha = random.randint(0,255)
-        self.x1 = random.randint(0,400)
-        self.y1 = random.randint(0,400)
-        self.x2 = random.randint(0,400)
-        self.y2 = random.randint(0,400)
+        super().__init__()
 
     def draw(self, base):
         #canvas of size 350 * 350
-        drw = ImageDraw.Draw(base,'RGBA')
-        coordinate = [(self.x1, self.y1),(self.x2, self.y2)]
-        color = (self.red, self.green, self.blue, self.alpha)
+        super().draw(base)
         drw.rectangle(coordinate, color, None)
         return base
 
 
-class Triangle():
+class Triangle(Shape):
 
     def __init__(self):
-        self.red = random.randint(0,255)
-        self.blue = random.randint(0,255)
-        self.green = random.randint(0,255)
-        self.alpha = random.randint(0,255)
-        self.x1 = random.randint(0,400)
-        self.y1 = random.randint(0,400)
-        self.x2 = random.randint(0,400)
-        self.y2 = random.randint(0,400)
+        super().__init__()
         self.x3 = random.randint(0,400)
         self.y3 = random.randint(0,400)
 
     def draw(self, base):
-        #canvas of size 350 * 350
-        drw = ImageDraw.Draw(base,'RGBA')
-        coordinate = [(self.x1, self.y1),(self.x2, self.y2),(self.x3, self.y3)]
-        color = (self.red, self.green, self.blue, self.alpha)
+        super().draw(base)
+        coordinate.append((self.x3, self.y3))
         drw.polygon(coordinate, color, None)
         return base
+
 
 primitive = [Circle(), Rectangle(), Triangle()]
 
 def Chromosome(select):
     base = Image.new('RGB',(350,350), (255,255,255))
     chromosome = []
-    for i in range(100):
-        temp = type.__new__(primitive[select])
+    temp = primitive[select]
+    instanceOf = temp.__class__
 
-        chromosome.append(primitive[select])
+    for i in range(100):
+        temp = instanceOf()
+        chromosome.append(temp)
 
     for i in range(100):
         base = chromosome[i].draw(base)
@@ -83,7 +84,7 @@ def Chromosome(select):
 
 
 def main():
-    Chromosome(1)
+    Chromosome(2)
 
 if __name__ == '__main__':
     main()
